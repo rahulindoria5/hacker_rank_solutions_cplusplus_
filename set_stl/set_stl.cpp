3ddff593b9f53c0722930b49820cf1e6d3c90171
rahulindoria5 <rahulindoria2006@gmail.com>
2022-07-17 20:15:13 +0200

    set_stl added

---
+++ set_stl/set_stl.cpp
@@ -0,0 +1,50 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <set>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    
+    set<int> s;
+    
+    // enter the number of elements in set
+    int N;
+    cin >> N;
+    int element;   
+    int choice;
+    
+    for (int i = 0; i < N; i++)
+    {
+        cin >> choice;
+        
+        switch (choice) {
+        case 1: cin >> element;
+                s.insert(element);
+                break;
+        
+        case 2: cin >> element;
+                s.erase(element);
+                break;
+                
+        case 3: cin >> element;
+                set<int>::iterator itr=s.find(element);
+                if (itr != s.end())
+                    cout << "Yes" << endl;
+                else 
+                    cout << "No" << endl;
+                
+                break;
+                
+        }
+        
+    }  
+    return 0;
+}
+
+
+
